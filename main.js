11856fa0c2aab09163ac5ce8662792b538b9ff10
Łukasz Orzeł <lukaszorzell@gmail.com>
2022-08-21 10:58:44 +0200

    More code cleanup.

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 function coordinatesFunc(event) {
     let x = event.clientX;
-    let y = event.clientY;
 
     let window_x = window.innerWidth;
-    let window_y = window.innerHeight;
 
     let center = window_x / 2;
 
@@ -12,9 +10,8 @@ function coordinatesFunc(event) {
     let picklock_element = document.getElementById('picklock');
 
     document.getElementById('x-pos').innerText = x;
-    document.getElementById('y-pos').innerText = y;
     document.getElementById('window-x-pos').innerText = window_x;
-    document.getElementById('window-y-pos').innerText = x_from_center;
+    document.getElementById('x-from-center').innerText = x_from_center;
 
     if (x > center && -x_from_center < 100) {
         picklock_element.style.transform = `translateX(-50%) rotate(calc(${-x_from_center} * 1deg))`;
